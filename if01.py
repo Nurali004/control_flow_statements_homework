789d1d06a4d74e514ff33b098fc0ced49cb5d0d9
M A C W I N <mavzurovnurali@gmail.com>
2025-04-30 17:45:59 +0500

    sss

--- if01.py
+++ if01.py
@@ -1,9 +1,21 @@
 def main(a):
     """
-    If the number is positive, increase it to 1,otherwise leave unchanged.
+    If the number is positive, increase it by 1, otherwise leave unchanged.
     Args:
         a: integer
     Returns:
         a: a increased by 1 if positive, else unchanged.
     """
-    return
+    if a > 0:
+        return a + 1
+    else:
+        return a
+
+# Foydalanuvchidan son kiritiladi
+a = int(input("Sonni kiriting: "))
+print(main(a))  # Natijani chiqarish
+
+
+
+
+
