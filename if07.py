333e42c8107323ba434a91b4d8ca928d756b8a86
M A C W I N <mavzurovnurali@gmail.com>
2025-04-30 18:30:28 +0500

    777

--- if07.py
+++ if07.py
@@ -12,4 +12,17 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a==0:
+        return "zero"
+    elif a>0 and a%2==0:
+        return "juft raqam"
+    elif a>0 and a%2==1:
+        return "toq musbat raqam"
+
+    elif a<0 and a%2==0:
+        return "juft manfiy raqam"
+    else:
+        return "manfiy toq raqam"
+
+a=int(input("sonni kiriting:"))
+print(main(a))
